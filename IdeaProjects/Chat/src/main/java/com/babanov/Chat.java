fc9edc1dd1045a9ed5f6d16f88036d3418cc4b06
GerrAlt12 <loky2003@gmail.com>
2016-04-15 11:50:34 +0300

    123

---
+++ IdeaProjects/Chat/src/main/java/com/babanov/Chat.java
@@ -0,0 +1,21 @@
+package com.babanov;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by st005965 on 08.04.2016.
+ */
+public class Chat extends HttpServlet {
+//123
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
